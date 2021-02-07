09e85077550a24abf2cdc25fa906668d5f806158
Dennis R. Friedrichsen <dennis.r.friedrichsen@gmail.com>
2021-02-06 18:14:34 -0600

    add sudo

--- build/nodebug-kernel.sh
+++ build/nodebug-kernel.sh
@@ -4,5 +4,5 @@ LOG_FILE=~/logs/nodebug-kernel_$(date +"%FT%T").log
 
 cd /usr/src
 date >> $LOG_FILE
-nohup time make -j2 kernel KERNCONF=GENERIC-NODEBUG >> $LOG_FILE
+nohup sudo time make -j2 kernel KERNCONF=GENERIC-NODEBUG >> $LOG_FILE
 date >> $LOG_FILE
