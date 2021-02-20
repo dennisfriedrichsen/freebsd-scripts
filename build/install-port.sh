e4a80d24e63b0239f8a2cc3b47d4e9269da99ffc
Dennis R. Friedrichsen <dennis.r.friedrichsen@gmail.com>
2021-02-20 14:55:25 -0600

    start of a script for installing ports

---
+++ build/install-port.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+PORT=$1
+FILE = echo $PORT | sed 's#/#_#g'
+LOG_FILE=~/logs/$FILE_$(date +"%FT%T").log
+
+date >> $LOG_FILE
+nohup sudo time portmaster -G --no-confirm $PORT >> $LOG_FILE
+date >> $LOG_FILE
