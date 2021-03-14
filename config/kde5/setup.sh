748b2213cddd56503352e4f25db6931599668524
Dennis R. Friedrichsen <dennis.r.friedrichsen@gmail.com>
2021-03-14 10:27:04 -0500

    fix the fstab addition

--- config/kde5/setup.sh
+++ config/kde5/setup.sh
@@ -1,6 +1,6 @@
 #! /bin/csh
 
-sudo echo "proc  /proc   procfs  rw  0   0" >> /etc/fstab
+echo "proc  /proc   procfs  rw  0   0" | sudo tee -a /etc/fstab
 sudo sysrc dbus_enable=YES
 sudo sysrc hald_enable=YES
 sudo sysrc sddm_enable=YES
